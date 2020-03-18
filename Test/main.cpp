ecd8cc7e0c25e37a99562f0ee83fee4f90559d9a
Najbjerg <alexander190591@gmail.com>
2020-03-18 08:59:14 +0100

    Changes to Data class. Works properly now with all methods.

--- Test/main.cpp
+++ Test/main.cpp
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 
-TEST( Data, setMessage)
+
+TEST(Data, setMessage_getData)
 {
     IData* myDataPtr = new Data();
     myDataPtr->setMessage(ERROR_NO_ID_REGISTERED);
@@ -13,6 +14,53 @@ TEST( Data, setMessage)
 
     ASSERT_EQ(data[0], 'M');
     ASSERT_EQ((uint8_t)data[1], ERROR_NO_ID_REGISTERED);
+}
+
+TEST(Data, setData_getData)
+{
+    IData* myDataPtr = new Data();
+    char data[SIZE_OF_DATA_ARRAY] = {'D','1','2','3','4','5','6','7'};
+    myDataPtr->setData(data);
+    char dataRead[SIZE_OF_DATA_ARRAY];
+    myDataPtr->getData(dataRead);
+
+    bool isSame = false;
+    for(int i = 0; i < SIZE_OF_DATA_ARRAY; i++)
+    {
+        //std::cout << " data[" << i << "]: "<< data[i] << ". dataRead[" << i << "]: " << dataRead[i] << std::endl; // For visual testing...
+        if(data[i] == dataRead[i])
+            isSame = true;
+        else
+        {
+            isSame = false;
+            break;
+        }
+    }   
+    ASSERT_EQ(isSame, true);
+}
+
+TEST(Data, setIDdata_getData)
+{
+    IData* myDataPtr = new Data();
+    char IDdata[SIZE_OF_DATA_ARRAY] = {'1','2','3','4','5','6','7'};
+    char IDDataFinal[SIZE_OF_DATA_ARRAY] = {'D','1','2','3','4','5','6','7'};
+    myDataPtr->setIDdata(IDdata);
+    char dataRead[SIZE_OF_DATA_ARRAY];
+    myDataPtr->getData(dataRead);
+
+    bool isSame = false;
+    for(int i = 0; i < SIZE_OF_DATA_ARRAY; i++)
+    {
+        //std::cout << " IDDataFinal[" << i << "]: "<< IDDataFinal[i] << ". dataRead[" << i << "]: " << dataRead[i] << std::endl; // For visual testing...
+        if(IDDataFinal[i] == dataRead[i])
+            isSame = true;
+        else
+        {
+            isSame = false;
+            break;
+        }
+    }   
+    ASSERT_EQ(isSame, true);
 }
 
 int main( int argc, char* argv[] )
--- src/Protocol/Src/Data.cpp
+++ src/Protocol/Src/Data.cpp
@@ -15,6 +15,7 @@
  * @brief Method that sets the dataOutputArray to the values found in the _data array.
  * 
  * @param dataOutputArray Array that is being filled with the values found in the _data array.
+ *          OBS: Size of dataOutputArray MUST be SIZE_OF_DATA_ARRAY long!!
  */
 void Data::getData(char dataOutputArray[])
 {
@@ -26,6 +27,7 @@ void Data::getData(char dataOutputArray[])
  * @brief Method that sets the _data attribute in the Data object from the dataInputArray.
  * 
  * @param dataInputArray Array of which the values are copied into the _data array.
+ *           OBS: Size of dataInputArray MUST be SIZE_OF_DATA_ARRAY long!!
  */
 void Data::setData(char dataInputArray[])
 {
@@ -45,10 +47,16 @@ void Data::setMessage(Message msg)
     _data[1] = msg;
 }
 
+/**
+ * @brief Setting up the IDdata array with the ID values found in ID.
+ * 
+ * @param ID is the char array that holds the ID values.
+ *           OBS: Size of ID MUST be SIZE_OF_DATA_ARRAY-1 long!!
+ */
 void Data::setIDdata(char ID[])
 {
     _data[SIZE_OF_DATA_ARRAY] = {0};        // Clearing the array.
-    _data[0] = 'M';
+    _data[0] = 'D';
     for(int i = 1; i < SIZE_OF_DATA_ARRAY; i++)
-        _data[i] = ID[i];
+        _data[i] = ID[i-1];
 }
