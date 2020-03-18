34a65bb1e383d574c6521c0436bc91f6ef82b76e
Tobias Aunbøl <201609973@post.au.dk>
2020-03-18 15:05:25 +0100

    dummyScanner added NOW BUILDING

--- src/Modules/ScannerModule/include/DummyScanner.h
+++ src/Modules/ScannerModule/include/DummyScanner.h
@@ -1,1 +1,32 @@
-test test test
+/**
+ * @file DummyScanner.h
+ * @author Tobias Bach Aunbøl (au328752@post.au.dk)
+ * @brif Test class for the ScannerModule used for testing
+ * @version 0.1
+ * @date 2020-03-18
+ *
+ *
+ */
+
+#ifndef DUMMYSCANNER_H
+#define DUMMYSCANNER_H
+
+#include "IScannerModule.h"
+
+#define SIZE_OF_DATA 7
+
+
+class DummyScanner : public IScannerModule
+{
+public:
+	DummyScanner();
+	void scan();
+	unsigned char* retrieveResult();
+	bool isNewScanAvailable();
+	int getSizeOfData();
+private:
+};
+
+
+
+#endif // DUMMYSCANNER_H
--- src/Modules/ScannerModule/src/DummyScanner.cpp
+++ src/Modules/ScannerModule/src/DummyScanner.cpp
@@ -1,1 +1,32 @@
-test test test
+/**
+ * @file DummyScanner.cpp
+ * @author Tobias Bach Aunbøl (au328752@post.au.dk)
+ * @brif Test class for the ScannerModule used for testing
+ * @version 0.1
+ * @date 2020-03-18
+ *
+ *
+ */
+
+#include "../Include/DummyScanner.h"
+
+DummyScanner::DummyScanner(void)
+{
+
+}
+void DummyScanner::scan()
+{
+
+}
+unsigned char* DummyScanner::retrieveResult()
+{
+
+}
+bool DummyScanner::isNewScanAvailable()
+{
+
+}
+int DummyScanner::getSizeOfData()
+{
+
+}
