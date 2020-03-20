302087dd5953a25b5d21a90178423a82ad6f5b1c
Tobias Aunbøl <201609973@post.au.dk>
2020-03-20 10:12:25 +0100

    Dummy Scanner updated

--- src/Modules/ScannerModule/include/DummyScanner.h
+++ src/Modules/ScannerModule/include/DummyScanner.h
@@ -1,6 +1,6 @@
 /**
  * @file DummyScanner.h
- * @author Tobias Bach Aunbøl (au328752@post.au.dk)
+ * @author Tobias Bach Aunboel (au328752@post.au.dk)
  * @brif Test class for the ScannerModule used for testing
  * @version 0.1
  * @date 2020-03-18
@@ -10,8 +10,14 @@
 
 #ifndef DUMMYSCANNER_H
 #define DUMMYSCANNER_H
+/**
+ *
+ * REMEMBER TO RUN Serial.begin(9600) from main otherwise print message wont work
+ *
+ */
 
 #include "IScannerModule.h"
+#include <Arduino.h>
 
 #define SIZE_OF_DATA 7
 
@@ -24,7 +30,10 @@ public:
 	unsigned char* retrieveResult();
 	bool isNewScanAvailable();
 	int getSizeOfData();
+	void init();
 private:
+	bool _isNewScanAvailable = false;
+	unsigned char _data[SIZE_OF_DATA] = {0};
 };
 
 
